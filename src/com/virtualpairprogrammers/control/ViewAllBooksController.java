cf936e25bc8f61d85f1872d9e709d36dadda0366
Mats Bergstroem <hackingtrom@gmail.com>
2018-05-02 16:14:25 +0200

    Use Spring annotations to wire up the controllers
    Instead of manually wire the controllers in the Dispatcher-servlet.xml
    file we can use Spring's  'Automatic Controller Scanning'.
    
    For this to work we need a minimal config in Dispatcher-servelet.xml
    and add a few Spring Annotations to each controller class that we
    want to be automatically wired.
    - @Controller : for class
    - @Autowired : for each dependency in the class (Dependency Injection)

--- src/com/virtualpairprogrammers/control/ViewAllBooksController.java
+++ src/com/virtualpairprogrammers/control/ViewAllBooksController.java
@@ -2,14 +2,18 @@ package com.virtualpairprogrammers.control;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.virtualpairprogrammers.domain.Book;
 import com.virtualpairprogrammers.services.BookService;
 
+@Controller
 public class ViewAllBooksController {
 
+	@Autowired
 	private BookService bookService;
 
 	public void setBookService(BookService bookService) {
