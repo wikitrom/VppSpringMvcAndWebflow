2c836c4d10b0795790b8bba30068042804271767
Mats Bergstroem <hackingtrom@gmail.com>
2018-05-02 11:43:01 +0200

    Add a Spring Dispatcher Servlet controller to dislay all books

---
+++ src/com/virtualpairprogrammers/control/ViewAllBooksController.java
@@ -0,0 +1,24 @@
+package com.virtualpairprogrammers.control;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.virtualpairprogrammers.domain.Book;
+import com.virtualpairprogrammers.services.BookService;
+
+public class ViewAllBooksController {
+
+	private BookService bookService;
+
+	public void setBookService(BookService bookService) {
+		this.bookService = bookService;
+	}
+	
+	@RequestMapping("/viewAllBooks")
+	public ModelAndView viewAllBooks() {
+		List<Book> allBooks = bookService.getEntireCatalogue();
+		return new ModelAndView("/displayAllBooks.jsp", "allBooks", allBooks);
+	}
+}
