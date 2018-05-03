5f606bec2974d9b98f3a0f625f04a8d63125ed80
Mats Bergstroem <hackingtrom@gmail.com>
2018-05-03 09:52:01 +0200

    Clean-up: Merge the two controllers in to one single class

--- src/com/virtualpairprogrammers/control/BookManagementController.java
+++ src/com/virtualpairprogrammers/control/BookManagementController.java
@@ -5,24 +5,28 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.virtualpairprogrammers.domain.Book;
 import com.virtualpairprogrammers.services.BookService;
 
 @Controller
-public class ViewAllBooksController {
+public class BookManagementController {
 
 	@Autowired
 	private BookService bookService;
 
-	public void setBookService(BookService bookService) {
-		this.bookService = bookService;
-	}
-	
 	@RequestMapping("/viewAllBooks")
 	public ModelAndView viewAllBooks() {
 		List<Book> allBooks = bookService.getEntireCatalogue();
 		return new ModelAndView("/displayAllBooks.jsp", "allBooks", allBooks);
 	}
+
+	@RequestMapping("/findByAuthor")
+	public ModelAndView findByAuthor(@RequestParam("AUTHOR") String author) {
+		List<Book> foundBooks = bookService.getAllBooksByAuthor(author);
+		return new ModelAndView("/displayAllBooks.jsp", "allBooks", foundBooks);
+	}
+
 }
